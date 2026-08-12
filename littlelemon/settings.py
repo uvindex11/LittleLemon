ef7762ed0807c1be8c8586c1f6359eb4ce1fe40f
uvindex11 <fanobrubeck@gmail.com>
2026-08-12 11:15:14 -0500

    Update models, admin, settings, migration

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -73,10 +73,19 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# settings.py
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reservations', #ALTNAME 'LittleLemon'
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
